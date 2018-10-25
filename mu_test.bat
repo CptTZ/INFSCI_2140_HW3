58fa875240232becca2a9f3946473e0b3ea25e8b
TonyZ <zyc@gis4.org>
2018-10-25 16:53:01 -0400

    MU tests

--- mu_test.bat
+++ mu_test.bat
@@ -1,15 +1,3 @@
 @echo off
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 0 > 0
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 10 > 10
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 200 > 200
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 400 > 400
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 600 > 600
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 800 > 800
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 1000 > 1000
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 1200 > 1200
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 1500 > 1500
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 1800 > 1800
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 2000 > 2000
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 3000 > 3000
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 4000 > 4000
-"C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe" -jar hw3-mu.jar 5000 > 5000
+REM "C:\Program Files\RedHat\java-1.8.0-openjdk-1.8.0.171-1\bin\java.exe"
+java -jar target\hw3-0.0.1-jar-with-dependencies.jar 100,200,300,400,500,600,700,800,900,1000,1200,1400,1600,1800,2000,3000,4000,5000 > mu_tests
--- src/main/java/HW3Main.java
+++ src/main/java/HW3Main.java
@@ -4,6 +4,7 @@ import IndexingLucene.MyIndexReader;
 import Search.ExtractQuery;
 import Search.QueryRetrievalModel;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,28 +19,32 @@ public class HW3Main {
 		// Initialization.
 		MyIndexReader ixreader = new MyIndexReader("trectext");
 		QueryRetrievalModel model = new QueryRetrievalModel(ixreader);
-		if (args.length == 1) model.setMu(Double.parseDouble(args[0]));
-		System.err.println("MU used is: " + model.getMu());
+		double[] muList = new double[]{2000.0};
+		if (args.length == 1)
+			muList = Arrays.stream(args[0].split(",")).mapToDouble(Double::parseDouble).toArray();
+		for (double mu : muList) {
+			model.setMu(mu);
+			System.out.println("MU used is: " + model.getMu());
+			// Extract the queries.
+			ExtractQuery queries = new ExtractQuery();
 
-		// Extract the queries.
-		ExtractQuery queries = new ExtractQuery();
-
-		long startTime = System.currentTimeMillis();
-		while (queries.hasNext()) {
-			Query aQuery = queries.next();
-			System.out.println(aQuery.GetTopicId() + "\t" + aQuery.GetQueryContent());
-			// Retrieve 20 candidate documents on the indexing.
-			List<Document> results = model.retrieveQuery(aQuery, 20);
-			if (results != null) {
-				int rank = 1;
-				for (Document result : results) {
-					System.out.println(aQuery.GetTopicId() + " Q0 " + result.docno() + " " + rank + " " + result.score() + " MYRUN");
-					rank++;
+			long startTime = System.currentTimeMillis();
+			while (queries.hasNext()) {
+				Query aQuery = queries.next();
+				System.out.println(aQuery.GetTopicId() + "\t" + aQuery.GetQueryContent());
+				// Retrieve 20 candidate documents on the indexing.
+				List<Document> results = model.retrieveQuery(aQuery, 20);
+				if (results != null) {
+					int rank = 1;
+					for (Document result : results) {
+						System.out.println(aQuery.GetTopicId() + " Q0 " + result.docno() + " " + rank + " " + result.score() + " MYRUN");
+						rank++;
+					}
 				}
 			}
+			long endTime = System.currentTimeMillis(); // end time of running code
+			System.out.println("\n\n4 queries search time: " + (endTime - startTime) / 60000.0 + " min");
 		}
-		long endTime = System.currentTimeMillis(); // end time of running code
-		System.out.println("\n\n4 queries search time: " + (endTime - startTime) / 60000.0 + " min");
 		ixreader.close();
 	}
 
